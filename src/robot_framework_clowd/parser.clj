626c7848045acf97348170cf9dc9f253d8210619
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-05 15:04:06 -0400

    Enhance parser to handle incomplete attr-val strings from RF and to trim the strings received

--- src/robot_framework_clowd/parser.clj
+++ src/robot_framework_clowd/parser.clj
@@ -15,21 +15,28 @@
     (assoc m :index (dec (Integer. (:index m)))) ; human-friendly coming in
     m))
 
+(defn clean-file-path
+  "Convert file separators"
+  [s]
+  (str/replace s "\\" "/"))
+
 (defn parse-descriptor
   "Given the arguments to a keyword from RobotFramework, parse the arguments string and return a data structure needed by clj-webdriver"
   [^String descriptor]
   (if (or (nil? descriptor) (empty? descriptor) (= "nil" (.toLowerCase descriptor)))
     nil
-    (let [descriptions (str/split descriptor #"(?<!\\),")]
+    (let [descriptions (remove empty? (str/split descriptor #"(?<!\\),"))]
       (-> (zipmap
            (for [desc descriptions]
              (-> (re-find #"^([^=]+)=" desc)
                  second
+                 str/trim
                  .toLowerCase
                  (str/replace "_" "-")
                  keyword))
            (for [desc descriptions]
              (-> (re-find #"^[^=]+=(.*?)$" desc)
                  second
+                 str/trim
                  parse-regex)))
           handle-nums))))
