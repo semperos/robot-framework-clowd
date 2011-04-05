a751deef36a209251d41b987c7b1f78b9aeba3dc
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-05 18:55:39 -0400

    Add documentation and correct params

--- src/robot_framework_clowd/keywords/list.clj
+++ src/robot_framework_clowd/keywords/list.clj
@@ -1,14 +1,16 @@
 (in-ns 'robot-framework-clowd.keywords)
 
 (defn get-ordered-list-items
-  [ul-descriptor]
-  (->> [:ol (parse-descriptor ul-descriptor), :li {}]
+  "For the ordered list described by `ol-descriptor`, retrieve the text of all items and separate them by three commas `,,,`"
+  [ol-descriptor]
+  (->> [:ol (parse-descriptor ol-descriptor), :li {}]
        (wd/find-them @rf-browser)
        (map wd/text)
        (interpose ",,,")
        (apply str)))
 
 (defn get-unordered-list-items
+  "For the unordered list described by `ul-descriptor`, retrieve the text of all items and separate them by three commas `,,,`"
   [ul-descriptor]
   (->> [:ul (parse-descriptor ul-descriptor), :li {}]
        (wd/find-them @rf-browser)
