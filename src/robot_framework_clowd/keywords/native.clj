ba07265b1c55dae5684df7c16692cd3f28acab14
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-05 18:37:27 -0400

    Add RF keyword for taking a screenshot of the entire screen

--- src/robot_framework_clowd/keywords/native.clj
+++ src/robot_framework_clowd/keywords/native.clj
@@ -1,1 +1,11 @@
 (in-ns 'robot-framework-clowd.keywords)
+
+(defn capture-screenshot []
+  (let [size (.getScreenSize (Toolkit/getDefaultToolkit))
+        rect (Rectangle. 0 0 (.width size) (.height size))
+        robot (Robot.)
+        img (.createScreenCapture robot rect)
+        path (str (rand-int 100000) "_screenshot.png")
+        file (java.io.File. path)]
+    (do
+      (ImageIO/write img "png" file))))
