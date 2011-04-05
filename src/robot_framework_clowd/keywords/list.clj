7a0c149c4e159c763e62d7058e7452df21df015f
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-05 18:37:17 -0400

    Add RF keywords for getting the text of list items

--- src/robot_framework_clowd/keywords/list.clj
+++ src/robot_framework_clowd/keywords/list.clj
@@ -1,1 +1,17 @@
 (in-ns 'robot-framework-clowd.keywords)
+
+(defn get-ordered-list-items
+  [ul-descriptor]
+  (->> [:ol (parse-descriptor ul-descriptor), :li {}]
+       (wd/find-them @rf-browser)
+       (map wd/text)
+       (interpose ",,,")
+       (apply str)))
+
+(defn get-unordered-list-items
+  [ul-descriptor]
+  (->> [:ul (parse-descriptor ul-descriptor), :li {}]
+       (wd/find-them @rf-browser)
+       (map wd/text)
+       (interpose ",,,")
+       (apply str)))
