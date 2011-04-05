8d4d384ca70787d3495b35afe3a7c9851e9b852b
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-05 15:05:02 -0400

    Add support for RF keywords that manipulate HTML file fields

--- src/robot_framework_clowd/keywords/file_field.clj
+++ src/robot_framework_clowd/keywords/file_field.clj
@@ -1,1 +1,13 @@
 (in-ns 'robot-framework-clowd.keywords)
+
+;; ## Actions
+
+(defn choose-file
+  "Provide the path for a local file to the specified filefield form element"
+  [descriptor local-file-path]
+  (browser-> :filefield descriptor (wd/send-keys (clean-file-path local-file-path))))
+
+(defn get-filefield-path
+  "Get the path present in the specified filefield form element"
+  [descriptor]
+  (browser-> :filefield descriptor wd/value))
