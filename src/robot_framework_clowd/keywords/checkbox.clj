f02bb2074182d63045ad4b46832547c8a5e23872
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-05 10:47:55 -0400

    Add checkbox handling RF keywords

--- src/robot_framework_clowd/keywords/checkbox.clj
+++ src/robot_framework_clowd/keywords/checkbox.clj
@@ -1,1 +1,30 @@
 (in-ns 'robot-framework-clowd.keywords)
+
+;; ## Actions
+
+(defn select-checkbox
+  "Select the specified checkbox"
+  [descriptor]
+  (browser-> :checkbox descriptor wd/select))
+
+(defn deselect-checkbox
+  "Deselect the specified checkbox"
+  [descriptor]
+  (browser-> :checkbox descriptor wd/deselect))
+
+(defn toggle-checkbox
+  "If a checkbox is selected, make it deselected, and vice versa"
+  [descriptor]
+  (browser-> :checkbox descriptor wd/toggle))
+
+;; ## Conditions
+
+(defn checkbox-should-be-selected
+  "Verify that the specified checkbox is selected"
+  [descriptor]
+  (throw-not-selected :checkbox descriptor))
+
+(defn checkbox-should-not-be-selected
+  "Verify that the specified checkbox is not selected"
+  [descriptor]
+  (throw-selected :checkbox descriptor))
