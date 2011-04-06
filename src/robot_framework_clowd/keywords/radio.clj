18e81fc86f78faa51cd4dff8c3f3233cd92d22e5
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-06 00:10:46 -0400

    Add RF keywords to support radio buttons

--- src/robot_framework_clowd/keywords/radio.clj
+++ src/robot_framework_clowd/keywords/radio.clj
@@ -1,1 +1,20 @@
 (in-ns 'robot-framework-clowd.keywords)
+
+;; ## Actions
+
+(defn select-radio-button
+  "Select the specified radio button"
+  [descriptor]
+  (browser-> :radio descriptor wd/select))
+
+;; ## Conditions
+
+(defn radio-button-should-be-selected
+  "Verify that the specified radio button form element is selected"
+  [descriptor]
+  (throw-not-selected :radio descriptor))
+
+(defn radio-button-should-not-be-selected
+  "Verify that the specified radio button form element is selected"
+  [descriptor]
+  (throw-selected :radio descriptor))
