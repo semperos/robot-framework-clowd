6d9686b6da31eff204bbac5a1fbd192de3a989d9
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-07 17:18:31 -0400

    Temporarily disalbe robot-remote-server, use via checkouts; add RF jar as dev dep

--- project.clj
+++ project.clj
@@ -2,9 +2,10 @@
   :description "RobotFramework keyword library for web testing written in Clojure leveraging Selenium-WebDriver"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [necessary-evil "1.1.0"]
-                 [robot-remote-server "0.2.3"]
+               ;;  [robot-remote-server "0.2.4"]
                  [org.seleniumhq.selenium/selenium-server "2.0b3"]
                  [clj-webdriver "0.2.2"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-                     [marginalia "0.5.0"]]
+                     [marginalia "0.5.0"]
+                     [org.robotframework/robotframework "2.5.5"]]
   :main robot-framework-clowd.keywords)
