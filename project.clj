a20902ee195990a69667ba9ae62776c3e260c70e
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-07 08:04:23 -0400

    Update clj-webdriver dep to 0.2.2, start using jar instead of checkouts

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [necessary-evil "1.1.0"]
                  [robot-remote-server "0.2.3"]
-                 [org.seleniumhq.selenium/selenium-server "2.0b2"]
-                 ;;[clj-webdriver "0.2.1"] using /checkouts currently
-                 ]
+                 [org.seleniumhq.selenium/selenium-server "2.0b3"]
+                 [clj-webdriver "0.2.2"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [marginalia "0.5.0"]]
   :main robot-framework-clowd.keywords)
