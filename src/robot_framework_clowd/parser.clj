f7db0771b97e45ffc386fc4217b7d9fee918bd1f
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-07 07:44:41 -0400

    Add clj-webdriver requirement for handling select-list param parsing

--- src/robot_framework_clowd/parser.clj
+++ src/robot_framework_clowd/parser.clj
@@ -1,5 +1,6 @@
 (ns robot-framework-clowd.parser
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clj-webdriver.core :as wd] :reload))
 
 (defn parse-regex
   "Given a value, detect whether or not it should be a regex and convert appropriately"
@@ -40,3 +41,41 @@
                  str/trim
                  parse-regex)))
           handle-nums))))
+
+(defn parse-item-selection
+  "Parse RF parameters for describing an item in a select list. Return vector, first item a fn for selecting, the second item the param to that fn"
+  [^String item-descriptor op]
+  (if (or (nil? item-descriptor) (empty? item-descriptor) (= "nil" (.toLowerCase item-descriptor)))
+    [wd/select-by-index 0] ; if nothing, default to picking first item in list
+    (let [descriptions (remove empty? (str/split item-descriptor #"(?<!\\),"))
+          desc-map (-> (zipmap
+                        (for [desc descriptions]
+                          (-> (re-find #"^([^=]+)=" desc)
+                              second
+                              str/trim
+                              .toLowerCase
+                              (str/replace "_" "-")
+                              keyword))
+                        (for [desc descriptions]
+                          (-> (re-find #"^[^=]+=(.*?)$" desc)
+                              second
+                              str/trim
+                              parse-regex)))
+                       handle-nums)]
+      (condp = op
+          :select (cond
+                   (contains? desc-map :index) [wd/select-by-index (Integer. (:index desc-map))] ; human-friendly index, 1-based
+                   (contains? desc-map :value) [wd/select-by-value (:value desc-map)]
+                   (contains? desc-map :text)  [wd/select-by-text (:text desc-map)]
+                   :else (throw (IllegalArgumentException.
+                                 (str "When selecting an element from a select list, "
+                                      "you may only use the 'index', 'value' or 'text' "
+                                      "attributes."))))
+          :deselect (cond
+                     (contains? desc-map :index) [wd/deselect-by-index (Integer. (:index desc-map))] ; human-friendly index, 1-based
+                     (contains? desc-map :value) [wd/deselect-by-value (:value desc-map)]
+                     (contains? desc-map :text)  [wd/deselect-by-text (:text desc-map)]
+                     :else (throw (IllegalArgumentException.
+                                   (str "When selecting an element from a select list, "
+                                        "you may only use the 'index', 'value' or 'text' "
+                                        "attributes."))))))))
