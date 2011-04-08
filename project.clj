a38ad37827c93abc2a0708021efc6e0d8441a0b6
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-08 11:40:12 -0400

    Bump version of robot-remote-server to 0.2.5, proper handling of RF fn args

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "RobotFramework keyword library for web testing written in Clojure leveraging Selenium-WebDriver"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [necessary-evil "1.1.0"]
-               ;;  [robot-remote-server "0.2.4"]
+                 [robot-remote-server "0.2.5"]
                  [org.seleniumhq.selenium/selenium-server "2.0b3"]
                  [clj-webdriver "0.2.2"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
